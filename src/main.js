0e622c3e7df4b15726025c67d478b1b6d48477d6
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-01-25 02:16:11 -0300

    add functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,55 @@
 import './style.css'
+
+let input = document.getElementById("input");
+let btns = document.getElementsByClassName("btn");
+let btnResult = document.getElementById("btn-equal");
+
+const operation = () => {
+  for (let btn of btns) {
+    btn.addEventListener("click", () => {
+      if (
+        btn.textContent !== "=" && input.textContent.length < 13 && input.textContent != 0
+      ) {
+        input.textContent += btn.textContent;
+      } else if (btn.textContent !== "=" && input.textContent.length < 13) {
+        input.textContent = btn.textContent;
+      }
+    });
+  }
+};
+operation();
+
+btnResult.addEventListener("click", () => {
+  let replacement = "";
+  if (input.textContent.includes("x")) {
+    replacement = input.textContent.replace("x", "*");
+    input.textContent = eval(replacement);
+  } else{
+    let result = eval(input.textContent);
+    input.textContent = result;
+  }
+  
+});
+
+
+
+
+
+// btnResult.addEventListener("click", () => {
+  
+//     if (input.textContent.includes("x")) {
+//       replacement = input.textContent.replace("x", "*");
+//       let result = new Function("return" + input.textContent.trim())();
+//       input.textContent = result;
+//     } else {
+//       let result = new Function("return " + input.textContent.trim())();
+//       input.textContent = result;
+//     }
+//   });
+
+
+
+
+input.addEventListener("click", () => {
+  input.textContent = 0;
+});
